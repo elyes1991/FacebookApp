5ef67d60ca6520ce04451cbeba2d1b18ea6ec8a8
elyes <>
2017-04-30 15:49:18 -0700

    Setting the project structure

---
+++ app/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Config;
+
+class Config {
+
+    const DB_HOST = '127.0.0.1';
+    const DB_USER = 'root';
+    const DB_PASS = '14021991';
+    const DB_NAME = 'facebook_app_db';
+    const FB_APP_ID = '1531160153803287';    
+    const FB_APP_SECRET = 'e0118e7a69842710c2d7ef0dae9dbb48';
+    const CALLBACK_URL = 'http://localhost.dev/index.php/show';
+    const FB_DEFAULT_GRAPH_VERSION = 'v2.2';
+
+}
