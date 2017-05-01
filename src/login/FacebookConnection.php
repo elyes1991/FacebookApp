8cd3d20d6d2a0da3d7bfffe6a801ba9384081f35
elyes <elyes@ubuntu>
2017-05-01 04:29:05 -0700

    Update FacebookConnectio Class.

--- src/login/FacebookConnection.php
+++ src/login/FacebookConnection.php
@@ -5,6 +5,9 @@ namespace Login;
 use Facebook\Facebook;
 use Config\Config;
 
+/**
+ * FacebookConnection class collect facebook profile data from facebook apis.
+ */
 class FacebookConnection {
 
     /**
@@ -32,6 +35,47 @@ class FacebookConnection {
         $helper = $this->fb->getRedirectLoginHelper();
         $loginUrl = $helper->getLoginUrl(Config::CALLBACK_URL, ['email']);
         return $loginUrl;
+    }
+
+    /**
+     * Get Facebook longLivedAccessToken.
+     * @return AccessToken
+     */
+    private function getLongLivedAccessToken() {
+        $helper = $this->fb->getRedirectLoginHelper();
+        $accessToken = $helper->getAccessToken();
+        $oAuth2Client = $this->fb->getOAuth2Client();
+        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+        $this->fb->setDefaultAccessToken($longLivedAccessToken);
+        return $longLivedAccessToken;
+    }
+
+    /**
+     * Get User.
+     * @return Models\User
+     */
+    public function getProfile() {
+
+        $longLivedAccessToken = $this->getLongLivedAccessToken();
+        $accessToken = (string) $longLivedAccessToken;
+        $expirationDate = $longLivedAccessToken->getExpiresAt();
+        if ($expirationDate != null) {
+            $tokenExpirationDate = $expirationDate->format('Y-m-d H:i:s');
+        }
+        $tokenExpirationDate = $longLivedAccessToken->getExpiresAt();
+
+        $this->fb->setDefaultAccessToken($accessToken);
+        $profile_request = $this->fb->get('/me?fields=name,picture');
+        $profile = $profile_request->getGraphNode()->asArray();
+        $user = new User();
+        $user->setData(array(
+            'name' => $profile['name'],
+            'imageUrl' => $profile['picture']['url'],
+            'facebookId' => $profile['id'],
+            'facebookToken' => $accessToken,
+            'tokenExpirationDate' => $tokenExpirationDate,
+        ));
+        return $user;
     }
 
 }
