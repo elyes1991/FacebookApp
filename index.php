923bfc4b550dd7d39a236871f21cf74af0ce2eab
elyes <elyes@ubuntu>
2017-05-01 04:38:35 -0700

    Updating the front controller

--- index.php
+++ index.php
@@ -5,18 +5,31 @@ require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 
 use Controllers\MainController;
+use Login\FacebookConnection;
+use Db\Database;
+use Models\UserRepository;
 
 $uri = $_SERVER['REQUEST_URI'];
 
-$mainController = new MainController();
+$facebookConnection = new FacebookConnection();
+$database = new Database();
+$userRepository = new UserRepository($database);
+$mainController = new MainController($facebookConnection, $userRepository);
+
 
 if ('/index.php' == $uri) {
 
     $mainController->loginAction();
-} elseif ('/index.php/show' == $uri) {
+    
+} elseif ('/index.php/show' == $uri || isset($_GET['code'])) {
 
     $mainController->showProfileAction();
+    if (isset($_GET['code'])) {
+        header('Location: ./show');
+    }
+    
 } else {
-
+    
     $mainController->renderErrorPage();
 }
+die();
