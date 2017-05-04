c840e71e724ec7d0e8266e7d650ebd9741f6cae6
elyes <elyes@ubuntu>
2017-05-04 01:31:45 -0700

    Creating database initialisation file

---
+++ init_db.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ComposerScript;
+
+use Db\Database;
+use Config\Config;
+
+class init_db
+{
+
+    /**
+     * Create the database.
+     */
+    public static function createDatabase()
+    {
+        $database = new Database();
+        $sql = "CREATE DATABASE " . Config::DB_NAME;
+        $database->execute($sql);
+    }
+
+    /**
+     * Create the User table.
+     */
+    public static function createUserTable()
+    {
+        $database = new Database();
+        $sql = "CREATE TABLE User (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                name VARCHAR(30) ,
+                imageUrl VARCHAR(2083),
+                facebookId VARCHAR(50),
+                facebookToken VARCHAR(2083),
+                tokenExpirationDate DATETIME,
+                created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                isActive boolean not null default 1
+                );
+                ALTER TABLE `User` ADD INDEX `facebook_id` (`facebookId`);
+                ALTER TABLE `User` ADD INDEX `user_id` (`id`);";
+        $database->execute($sql);
+    }
+}
